46775074028a28c71afa2bf5a896f9e073eae003
fernandofernandessimao <fernandofernandessimao@gmail.com>
2021-11-16 21:23:52 +0100

    english corretion

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,7 @@ export default function About() {
   return (
     <div style={{ textAlign: "center" }}>
       <p style={{ padding: "15px", textAlign: "justify" }}>
-        First portfolio project for Full Stack Developer. This actual version is
-        a conceptual idea, as it needs real data and models from supermakets or
-        stores. Althought conceptual, it can be applied (and actually it is) to
-        any store and wharehouse.
+      First portfolio project for Full Stack Developer. This actual version is a conceptual idea, as it needs real data and models from supermarkets or stores. Although conceptual, it can be applied (and actually it is) to any store and warehouse.
       </p>
     </div>
   );
